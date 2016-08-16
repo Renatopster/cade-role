6da348053c31cfd6a0145642930546c66b7f634d
Renato Chaves <renato.g.chaves@gmail.com>
2016-08-16 18:54:39 -0300

    Remove obsolete import

--- app/shared/tabs/tabs.component.ts
+++ app/shared/tabs/tabs.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {EventsPage} from '../../events/events-page/events-page.component';
 import {MapPage} from '../../map/map-page/map-page.component';
-import {SearchPage} from '../../search/search-page.component';
 
 @Component({
   templateUrl: 'build/shared/tabs/tabs.component.html'
