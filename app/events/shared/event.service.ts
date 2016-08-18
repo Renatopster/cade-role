a21d6d5befacbe2f7f82f22d881c7bbc9d383413
Renato Chaves <renato.g.chaves@gmail.com>
2016-08-18 00:35:40 -0300

    Filter events starting on the next 48 hours

--- app/events/shared/event.service.ts
+++ app/events/shared/event.service.ts
@@ -14,7 +14,9 @@ export class EventService {
     return new Promise(resolve => {
 
       // resolve(EVENTS);
-      this.http.get('http://localhost:3000/events?lat=-23.5502055&lng=-46.6537139&distance=500&accessToken=861161564018485|37gRLbgrtmX_SFIbaqUzDzmbJd4&sort=popularity')
+      var until = new Date();
+      until.setDate(until.getDate()+2);
+      this.http.get('http://localhost:3000/events?lat=-23.5502055&lng=-46.6537139&distance=500&accessToken=861161564018485|37gRLbgrtmX_SFIbaqUzDzmbJd4&sort=popularity&until='+until.toISOString())
         .map(res => res.json())
         .subscribe(data => {
           resolve(data.events);
