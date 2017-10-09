c35de992092b17807d532e70bfff4823a169a3e6
Renato Chaves <renato.g.chaves@gmail.com>
2017-10-08 23:11:06 -0300

    Replace FB events fetcher

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,11 +1,12 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
-import {ReplaySubject, Observable} from 'rxjs';
-import LatLng = google.maps.LatLng;
+import {Observable, ReplaySubject} from 'rxjs';
 import {LocationService} from './location.service';
 import {DateInterval} from '../shared/DateInterval';
 import {Event} from '../events/Event';
+import * as EventSearch from 'facebook-events-by-location-core'
+import LatLng = google.maps.LatLng;
 
 @Injectable()
 export class EventService {
@@ -20,7 +21,8 @@ export class EventService {
 
   private dateInterval: DateInterval;
 
-  constructor(private http: Http, private locationService: LocationService) {
+  constructor(private http: Http,
+              private locationService: LocationService) {
     var since = new Date();
     var until = new Date();
     until.setDate(until.getDate() + 2);
@@ -47,48 +49,57 @@ export class EventService {
     return this.dateInterval
   }
 
-  refreshEvents() {
-    var since = this.dateInterval.since;
-    var until = this.dateInterval.until;
-    var center = this.center;
-    var distance = 1000;
+  async refreshEvents() {
+    const since = this.dateInterval.since;
+    const until = this.dateInterval.until;
+    const center = this.center;
+    const distance = 1000;
 
-    var lat = center.lat();
-    var lng = center.lng();
+    const lat = center.lat();
+    const lng = center.lng();
 
-    return new Promise<Event[]>(resolve => {
-      this.loading.next(true);
-      this.http.get('http://cade-role.rgchaves.com.br/events?lat=' + lat + '&lng=' + lng + '&distance=' + distance + '&sort=popularity&since=' + since.toISOString() + '&until=' + until.toISOString())
-        .map(res => res.json().events.map((data) => {
-          return <Event>{
-            id: data.id,
-            name: data.name,
-            description: data.description,
-            profilePictureUrl: data.profilePicture,
-            coverPictureUrl: data.coverPicture,
-            attendingCount: data.stats.attending,
-            maybeCount: data.stats.maybe,
-            startTime: data.startTime,
-            endTime: data.endTime,
-            venueName: data.venue.name,
-            venueAddress: (data.venue.location ? data.venue.location.street + ' - ' + data.venue.location.city + '/' + data.venue.location.state : null),
-            latitude: data.venue.location.latitude,
-            longitude: data.venue.location.longitude
-          };
-        }))
-        .subscribe(events => {
-          this.loading.next(false);
-          events = events.sort(function (a, b) {
-            if (a.attendingCount < b.attendingCount) {
-              return 1;
-            } else if (a.attendingCount > b.attendingCount) {
-              return -1;
-            }
-            return 0;
-          });
-          this.events.next(events);
-          resolve(events);
-        });
-    });
+    const options = {
+      accessToken: '861161564018485|37gRLbgrtmX_SFIbaqUzDzmbJd4',
+      lat: lat,
+      lng: lng,
+      distance: distance,
+      sort: 'popularity',
+      since: since.toISOString(),
+      until: until.toISOString(),
+      categories: ['ARTS_ENTERTAINMENT'],
+    };
+    const es = new EventSearch()
+
+    this.loading.next(true);
+    const response = await es.search(options)
+    this.loading.next(false);
+
+    const events = response.events
+      .map((data) => {
+        return <Event>{
+          id: data.id,
+          name: data.name,
+          description: data.description,
+          profilePictureUrl: data.profilePicture,
+          coverPictureUrl: data.coverPicture,
+          attendingCount: data.stats.attending,
+          maybeCount: data.stats.maybe,
+          startTime: data.startTime,
+          endTime: data.endTime,
+          venueName: data.venue.name,
+          venueAddress: (data.venue.location ? data.venue.location.street + ' - ' + data.venue.location.city + '/' + data.venue.location.state : null),
+          latitude: data.venue.location.latitude,
+          longitude: data.venue.location.longitude
+        };
+      }).sort(function (a, b) {
+        if (a.attendingCount < b.attendingCount) {
+          return 1;
+        } else if (a.attendingCount > b.attendingCount) {
+          return -1;
+        }
+        return 0;
+      });
+    this.events.next(events);
+    return events
   }
 }
